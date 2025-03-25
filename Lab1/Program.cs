4abdab8d007f9490a89f078175415cf05d59a5da
teresav3 <tvargin@gmail.com>
2025-03-24 22:29:34 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Lab1;
+using System.Reflection.Metadata;
+
+namespace Lab1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Step 1
+        Console.WriteLine("Teresa Programming Logic");
+        /*Step 2 Teresa Vargin
+        IT-1050- Lab 1*/
+        
+        //Step 3
+       
+        int myNum = 7;
+        Console.WriteLine(myNum);
+       
+        string myText = "C#";
+        Console.WriteLine(myText);
+        
+        double myDoubleNum = 0.00;
+        Console.WriteLine (myDoubleNum);
+        
+        bool myBool = false;
+        Console.WriteLine(myBool);
+
+        //Step 5
+
+        string schoolname = "Cuyahoga Community College";
+        Console.WriteLine (schoolname);
+
     }
 }
-Added Lab1 console app
